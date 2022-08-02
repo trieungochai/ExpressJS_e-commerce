7c45d043308dfb3f03c14d47cc92d3f77a63d269
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-08-02 18:13:56 +0700

    #9 auth/login: login user

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -28,11 +28,36 @@ const registerUser = async (req, res) => {
   };
 
   attachCookiesToResponse({ res, tokenPayload });
-  
+
   return res.status(StatusCodes.CREATED).json({ tokenPayload });
 };
 
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please enter your email and password");
+  }
+  const loggedInUser = await User.findOne({ email });
+  if (!loggedInUser) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  // compare password
+  const isPasswordCorrect = await loggedInUser.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const tokenPayload = {
+    userId: loggedInUser._id,
+    userName: loggedInUser.name,
+    userRole: loggedInUser.role,
+  };
+
+  attachCookiesToResponse({ res, tokenPayload });
+
+  return res.status(StatusCodes.OK).json({ tokenPayload });
+};
 
 const logoutUser = async (req, res) => {};
 
